057455e37c2049622d23b56ccfd5f5bf6e983077
mudoker <huuquoc7603@gmail.com>
2025-02-12 23:00:53 +0700

    refactor: replace App.css and fonts.css with global.css for improved styling management

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 
 import App from './App.jsx';
 import { store } from './config/redux/store';
-import './index.css';
+import './global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
