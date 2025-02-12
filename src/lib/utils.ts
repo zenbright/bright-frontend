9f2ad4ce8514fd547867583b84fd81c6c0f5d46f
mudoker <huuquoc7603@gmail.com>
2025-02-12 22:48:15 +0700

    refactor: update to use tailwind v4 and add all shadcn components

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
-import { clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-export function cn(...inputs: any) {
-    return twMerge(clsx(inputs));
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,14 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    plugins: [react(), svgr()],
+    plugins: [
+        react(),
+        svgr(),
+        tailwindcss()
+    ],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
